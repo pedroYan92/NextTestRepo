8e8fab652abde72c3eb42226aaf2bdd5603f00ec
Pedro_Yanez <pedro.yanez@appertech.com.br>
2022-10-11 23:15:04 -0300

    feat: add props to navbar

--- components/Navbar/index.tsx
+++ components/Navbar/index.tsx
@@ -1,12 +1,27 @@
-export const Navbar: React.FC = () => {
+type NavbarProps = {
+  menu1: string;
+  menu2: string;
+  menu3: string;
+  color?: string;
+  bgColor?: string;
+  logoColor?: string;
+};
+export const Navbar = ({
+  menu1,
+  menu2,
+  menu3,
+  color = "text-amber-500",
+  bgColor = "bg-slate-900",
+  logoColor = "text-amber-500",
+}: NavbarProps) => {
   return (
     <div className="px-32">
-      <nav className="bg-slate-900 h-10 flex items-center text-amber-500">
-        <h1 className="font-black text-amber-500 ml-4">Logo</h1>
-        <ul className="flex  ml-auto">
-          <li className="cursor-pointer mr-4">About</li>
-          <li className="cursor-pointer mr-4">Services</li>
-          <li className="cursor-pointer mr-4">Contact</li>
+      <nav className={`${bgColor} h-10 flex items-center `}>
+        <h1 className={`font-black ${logoColor} ml-4`}>Logo</h1>
+        <ul className={`flex ml-auto ${color}`}>
+          <li className="cursor-pointer mr-4">{menu1}</li>
+          <li className="cursor-pointer mr-4">{menu2}</li>
+          <li className="cursor-pointer mr-4">{menu3}</li>
         </ul>
       </nav>
     </div>
