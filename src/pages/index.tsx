301cb264912b58efcb8f7d318ce6fb713af85b0b
Pedro_Yanez <pedro.yanez@appertech.com.br>
2022-10-24 21:36:00 -0300

    teste commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,15 +3,17 @@ import { Navbar } from '../components/Navbar';
 
 const Home: NextPage = () => {
     return (
-        <div className="w-full">
-            <Navbar
-                menu1="About"
-                menu2="Last section"
-                menu3="Contact"
-                color="text-red-500"
-                logoColor="text-gray-200"
-            />
-        </div>
+        <>
+            <div className="w-full">
+                <Navbar
+                    menu1="About"
+                    menu2="Last section"
+                    menu3="Contact"
+                    color="text-red-500"
+                    logoColor="text-gray-200"
+                />
+            </div>
+        </>
     );
 };
 
